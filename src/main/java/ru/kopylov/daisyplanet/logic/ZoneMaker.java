6d17a504a74ae56c14d962e84fa2480b6015834a
sv-kopylov <imber81@gmail.com>
2018-06-06 08:02:40 +0300

    Refactoring ZoneMaker under J8

--- src/main/java/ru/kopylov/daisyplanet/logic/ZoneMaker.java
+++ src/main/java/ru/kopylov/daisyplanet/logic/ZoneMaker.java
@@ -1,5 +1,10 @@
 package ru.kopylov.daisyplanet.logic;
+
 import ru.kopylov.daisyplanet.model.Zone;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Created by sergey on 17.08.17.
@@ -7,34 +12,38 @@ import ru.kopylov.daisyplanet.model.Zone;
 public class ZoneMaker {
 
 
-    private static Zone makeZone(double radius, int halfZonation, int index){
-        double height = radius/halfZonation;
+    private static Zone makeZone(double radius, int halfZonation, int index) {
+        double height = radius / halfZonation;
         double latitude = calcLatitude(radius, height, index);
         double effectiveArea = calcZoneEffeciveArea(radius, height, index);
         return new Zone(latitude, effectiveArea, height);
 
     }
-    public static Zone[] makeZones(double radius, int halfZonation){
+
+    public static Zone[] makeZones(double radius, int halfZonation) {
         Zone[] rezult = new Zone[halfZonation];
-        for (int i=0; i<halfZonation; i++) {
-            rezult[i] = makeZone(radius, halfZonation, i);
-        }
+        IntStream
+                .range(0, halfZonation)
+                .parallel()
+                .forEach(i -> rezult[i] = makeZone(radius, halfZonation, i));
         return rezult;
     }
-    private static double calcZoneEffeciveArea(double radius, double height, int index){
+
+    private static double calcZoneEffeciveArea(double radius, double height, int index) {
 
         double alphaN = calcLatitude(radius, height, index);
-        double alphaN_PlusOne = calcLatitude(radius, height, index+1);
+        double alphaN_PlusOne = calcLatitude(radius, height, index + 1);
         double alphaDiff = alphaN_PlusOne - alphaN;
 
-        double sRectangle = height*radius*Math.cos(alphaN_PlusOne);
-        double sTriangle = 0.5*height*radius*(Math.cos(alphaN) - Math.cos(alphaN_PlusOne));
-        double sSegment = 0.5*radius*radius*(alphaDiff - Math.sin(alphaDiff));
+        double sRectangle = height * radius * Math.cos(alphaN_PlusOne);
+        double sTriangle = 0.5 * height * radius * (Math.cos(alphaN) - Math.cos(alphaN_PlusOne));
+        double sSegment = 0.5 * radius * radius * (alphaDiff - Math.sin(alphaDiff));
 
-        return 2*(sRectangle+sTriangle+sSegment);
+        return 2 * (sRectangle + sTriangle + sSegment);
     }
-    private static double calcLatitude(double radius, double height, int index){
-        return Math.asin((height*index)/radius);
+
+    private static double calcLatitude(double radius, double height, int index) {
+        return Math.asin((height * index) / radius);
     }
 
 
